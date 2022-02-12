c450e64c5eac3cc7adb1881415b21b1c937837ca
David <dav.gruber@htlstp.at>
2022-02-12 17:39:02 +0100

    Semesterstand

--- src/RoboBall/RoboBall.ino
+++ src/RoboBall/RoboBall.ino
@@ -16,7 +16,7 @@ class Motor {
     int ch1;
     int ch2;
     int Speed;
-    
+
   public:
     Motor() {}
     Motor(int, int, int, int);
@@ -40,22 +40,22 @@ Motor::Motor(int p1, int p2, int freq, int res) {
 }
 
 void Motor::drive() {
-  if( Speed == 0 ) {
+  if ( Speed == 0 ) {
     digitalWrite(pin1, LOW);
     digitalWrite(pin2, LOW);
-  } else if(Speed > 0) {
+  } else if (Speed > 0) {
     digitalWrite(pin2, LOW);
     ledcWrite(ch1, Speed);
-  } else if(Speed < 0) {
+  } else if (Speed < 0) {
     digitalWrite(pin1, LOW);
     ledcWrite(ch2, Speed * -1);
   }
 }
 
-void Motor::setspeed(int s) 
+void Motor::setspeed(int s)
 {
   Speed = s;
-  
+
 }
 
 void Motor::backgroundActivity()
@@ -64,15 +64,17 @@ void Motor::backgroundActivity()
   this->Speed -= 1;
 }
 
-Motor Right(13, 12, frequency, resolution);
-Motor Left(35, 32, frequency, resolution);
-String cmd;
+//Motor Right(35, 34, frequency, resolution);
+//Motor Left(13, 12, frequency, resolution);
+char command[30] = "";
+int cmdp = 0;
+int cmdavailable = false;
 
 void setup() {
   pinMode(13, OUTPUT);
   pinMode(12, OUTPUT);
+  pinMode(25, OUTPUT);
   pinMode(35, OUTPUT);
-  pinMode(32, OUTPUT);
 
   Serial.begin(115200);
   SerialBT.begin("ESPDavid"); //Bluetooth device name
@@ -80,19 +82,49 @@ void setup() {
 }
 
 void loop() {
-  
-  if (SerialBT.available()) {
+  int cmd;
+  if (SerialBT.available() && !cmdavailable ) {
     cmd = SerialBT.read();
-      if(cmd == "go") {
-        Right.setspeed(50);
-        Left.setspeed(50);
-        delay(1);
-      }
-      /*if(cmd == "stop") {
-        Right.Speed(0);
-        Left.Speed(0);
-      }*/
+    if (cmd == 10) {
+      command[cmdp] = '\0';
+      cmdp = 0;
+      cmdavailable = 1;
+    } else {
+      command[cmdp++] = (char)lowByte(cmd);
     }
-    Right.backgroundActivity();
-    Left.backgroundActivity();
+
+    if (cmdavailable) {
+      cmdavailable = 0;
+      Serial.println(command); 
+      if (command[0] == 'f') {
+        digitalWrite(25, HIGH);
+        digitalWrite(12, HIGH);
+      } else if(command[0] == 'l') {
+        digitalWrite(12, HIGH);
+      } else if(command[0] == 'r') {
+        digitalWrite(25, HIGH);  
+      } else if(command[0] == 'b') {
+        digitalWrite(35, HIGH);
+        digitalWrite(13, HIGH);
+      } else if(command[0] == 's') {
+        digitalWrite(25, LOW);
+        digitalWrite(12, LOW);
+        digitalWrite(35, HIGH);
+        digitalWrite(13, LOW);
+      }
+      
+      for(int i = 0; i < 30;i++) {
+        command[i] = '\0';
+      }
+    }
+
+
+
+    /*if(cmd == "stop") {
+      Right.Speed(0);
+      Left.Speed(0);
+      }*/
+  }
+  //Right.backgroundActivity();
+  //Left.backgroundActivity();
 }
