b752a49744daf81b834164dc050a5ac0ada7f632
David <dav.gruber@htlstp.at>
2022-03-18 08:07:44 +0100

    Motor testing

--- src/RoboBall/RoboBall.ino
+++ src/RoboBall/RoboBall.ino
@@ -1,139 +1,204 @@
 #include <BluetoothSerial.h>
 
+
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
-
-// --- CLASSES ---
+// --- MOTOR CLASS ---
 int lastchannel = 0;
 class Motor {
-  private:
-    int pin1;
-    int pin2;
-    int ch1;
-    int ch2;
-    int Speed;
+private:
+  int pin1;
+  int pin2;
+  int ch1;
+  int ch2;
+  int currentSpeed; //slow-follows the controlSpeed
 
-  public:
-    Motor() {}
-    Motor(int, int, int, int);
-    void drive();
-    void setspeed(int);
-    void backgroundActivity();
+public:
+  int controlSpeed; //controls the speed
+  static const int updatedelay = 2000; //time in micros (* 256 = ramptime = time to go from 0 to max)
+
+  Motor() {}
+  Motor(int, int, int, int);
+  void setSpeed(int);  
+  void drive();
+  void adjustCurrentSpeed();
 };
 
-
-Motor::Motor(int p1, int p2, int freq, int res) {
+Motor::Motor(int p1, int p2, int freq, int res)
+{
   int pin1 = p1;
   int pin2 = p2;
   int ch1 = lastchannel++;
   int ch2 = lastchannel++;
-  int Speed = 0;
+  int controlSpeed = 0; 
+  int currentSpeed = 0; 
+ 
+  
 
   ledcSetup(ch1, freq, res);
   ledcSetup(ch2, freq, res);
   ledcAttachPin(pin1, ch1);
   ledcAttachPin(pin2, ch2);
+
 }
 
-void Motor::drive() {
-  if ( Speed == 0 ) {
-    digitalWrite(pin1, LOW);
-    digitalWrite(pin2, LOW);
-  } else if (Speed > 0) {
-    digitalWrite(pin2, LOW);
-    ledcWrite(ch1, Speed);
-  } else if (Speed < 0) {
-    digitalWrite(pin1, LOW);
-    ledcWrite(ch2, Speed * -1);
+void Motor::adjustCurrentSpeed()
+{ 
+  //update currentSpeed
+  if(controlSpeed > currentSpeed) {
+    currentSpeed += 1;
+  } else if(controlSpeed < currentSpeed) {
+    currentSpeed -= 1;
   }
-
-  /*
-  ledcwrite(Speed)
-  */
+  //set new currentSpeed
+  if (currentSpeed == 0)
+  {
+    digitalWrite(pin1, LOW);
+    digitalWrite(pin2, LOW);
+  }
+  else if (currentSpeed > 0 && currentSpeed <= 256)
+  {
+    digitalWrite(pin2, LOW);
+    ledcWrite(ch1, currentSpeed);
+  }
+  else if (currentSpeed < 0 && currentSpeed >= -256)
+  {
+    digitalWrite(pin1, LOW);
+    ledcWrite(ch2, currentSpeed * -1);
+  }
 }
 
-void Motor::setspeed(int s)
+void Motor::setSpeed(int s)
 {
-  Speed = s;
-
+  controlSpeed = s;
 }
 
-void Motor::backgroundActivity()
+
+void Motor::drive()
 {
-  this->drive();
-  this->Speed -= 1;
+  if (controlSpeed == 0)
+  {
+    digitalWrite(pin1, LOW);
+    digitalWrite(pin2, LOW);
+  }
+  else if (controlSpeed > 0 && controlSpeed <= 256)
+  {
+    digitalWrite(pin2, LOW);
+    ledcWrite(ch1, currentSpeed);
+  }
+  else if (controlSpeed < 0 && controlSpeed >= -256)
+  {
+    digitalWrite(pin1, LOW);
+    ledcWrite(ch2, controlSpeed * -1);
+  }
 }
+
+
+
 
 // --- GLOBAL VARIABLES ---
+const int motorpins[] = {12,13,26,27};
 const int frequency = 5000;
 const int resolution = 8;
 int testchannel = 1;
-const int testpin = 14;
+const int testpin = 26;
 int devchannel = 2;
 const int devpin = 27;
-BluetoothSerial SerialBT;
 
-//Motor Right(35, 34, frequency, resolution);
-//Motor Left(13, 12, frequency, resolution);
+
+BluetoothSerial SerialBT;
+hw_timer_t * timer; // our timer
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+Motor motor;
+Motor mRight(motorpins[0], motorpins[1], frequency, resolution);
+Motor mLeft(motorpins[2], motorpins[3], frequency, resolution);
 
 char command[30] = "";
 int cmdp = 0;
 int cmdavailable = false;
+int spd;
 
+// --- onInterrupt ---
+void onIntervallUpdate() {
+  mRight.adjustCurrentSpeed();
+  mLeft.adjustCurrentSpeed();
+}
 
-void setup() {
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(25, OUTPUT);
-  pinMode(35, OUTPUT);
+// --- SETUP ---
+void setup()
+{
+  int i;
+  for(i = 0; i < 4; i++) {
+    pinMode(motorpins[i], OUTPUT);
+  }
 
+  pinMode(testpin, OUTPUT);
+  pinMode(devpin, OUTPUT);
   ledcSetup(testchannel, frequency, resolution);
   ledcAttachPin(testpin, testchannel);
   ledcSetup(devchannel, frequency, resolution);
-  testchannel += 1;
-  ledcAttachPin(devpin, testchannel);
-  testchannel -= 1;
-
+  ledcAttachPin(devpin, devchannel);
+  
+  timer = timerBegin(0, 240, true);
+  timerAttachInterrupt(timer, onIntervallUpdate, true);
+  timerAlarmWrite(timer, motor.updatedelay, true);
+  timerAlarmEnable(timer);
 
   Serial.begin(115200);
-  SerialBT.begin("RoboBall"); //Bluetooth device name
+  SerialBT.begin("RoboBall"); // Bluetooth device name
   Serial.println("The Roboter started, now you can pair it with bluetooth!");
+
+
 }
 
-
-void loop() {
+// --- LOOP ---
+void loop()
+{
   int cmd;
-  int speed;
-  if (SerialBT.available() && !cmdavailable ) {
+  // BT Handling -> Interrupt
+  if (SerialBT.available() && !cmdavailable)
+  {
     cmd = SerialBT.read();
-    if (cmd == 10) {
-      command[cmdp-1] = '\0';
+    if (cmd == 10)
+    {
+      command[cmdp - 1] = '\0';
       cmdp = 0;
       cmdavailable = 1;
-    } else {
+    }
+    else
+    {
       command[cmdp++] = (char)lowByte(cmd);
     }
   }
-  if (cmdavailable) {
+
+  // Command Handling
+  if (cmdavailable)
+  {
     cmdavailable = 0;
     Serial.println(command);
-    if (String(command) == "left") {
-      Serial.println("left");
-    }
-    speed = String(command).toInt();
-    Serial.println(speed);
-    if(speed >= 0) {
-      ledcWrite(testchannel, speed);
+    
+    spd = String(command).toInt();
+    Serial.println(spd);
+
+    mRight.setSpeed(spd);
+    // motor action
+    if (spd > 0)
+    {
+      ledcWrite(testchannel, spd);
       ledcWrite(devchannel, 0);
-    } else if(speed < 0) {
+    }
+    else if (spd < 0)
+    {
       ledcWrite(testchannel, 0);
-      ledcWrite(devchannel, speed*-1);
-    } else {
+      ledcWrite(devchannel, spd * -1);
+    }
+    else
+    {
       ledcWrite(testchannel, 0);
       ledcWrite(devchannel, 0);
     }
   }
-
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <BluetoothSerial/src/BluetoothSerial.h>
+#include <BluetoothSerial.h>
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
